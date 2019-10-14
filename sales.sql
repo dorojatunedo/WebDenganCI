71294ba0fc6c33874e30d62c1a6965c9906b3805
dorojatunedo <34097240+dorojatunedo@users.noreply.github.com>
2019-10-14 20:06:16 +0700

    Add files via upload

---
+++ sales.sql
@@ -0,0 +1,99 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 10.1.13-MariaDB : Database - sales
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sales` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `sales`;
+
+/*Table structure for table `barang` */
+
+DROP TABLE IF EXISTS `barang`;
+
+CREATE TABLE `barang` (
+  `kd_brg` varchar(5) NOT NULL,
+  `nm_brg` varchar(30) NOT NULL,
+  `satuan` varchar(20) NOT NULL,
+  `harga` double NOT NULL,
+  `stok` int(5) NOT NULL,
+  `stok_min` int(5) NOT NULL,
+  PRIMARY KEY (`kd_brg`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `barang` */
+
+insert  into `barang`(`kd_brg`,`nm_brg`,`satuan`,`harga`,`stok`,`stok_min`) values ('B01','Pensil','Box',25000,20,2),('B02','Penggaris','Pieces',7500,30,5),('B03','Penghapus','Dozen',50000,50,5),('B04','Buku Gambar','Pieces',5000,30,4),('B05','Pulpen','Pieces',5000,34,2);
+
+/*Table structure for table `djual` */
+
+DROP TABLE IF EXISTS `djual`;
+
+CREATE TABLE `djual` (
+  `no_jual` varchar(10) NOT NULL,
+  `kd_brg` char(6) NOT NULL,
+  `harga_jual` double NOT NULL,
+  `jml_jual` int(4) NOT NULL,
+  PRIMARY KEY (`no_jual`,`kd_brg`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `djual` */
+
+/*Table structure for table `goods` */
+
+DROP TABLE IF EXISTS `goods`;
+
+CREATE TABLE `goods` (
+  `goods_id` varchar(5) NOT NULL,
+  `goods_name` varchar(30) NOT NULL,
+  `unit` varchar(20) NOT NULL,
+  `price` double NOT NULL,
+  `goods_stock` int(5) NOT NULL,
+  `minimal_stock` int(5) NOT NULL,
+  PRIMARY KEY (`goods_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `goods` */
+
+/*Table structure for table `jual` */
+
+DROP TABLE IF EXISTS `jual`;
+
+CREATE TABLE `jual` (
+  `no_jual` varchar(10) NOT NULL,
+  `kd_kons` char(6) NOT NULL,
+  `tgl_jual` date NOT NULL,
+  PRIMARY KEY (`no_jual`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `jual` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(4) NOT NULL,
+  `user_id` varchar(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `password` varchar(254) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`user_id`,`name`,`password`) values (1,'admin','Administrator','0192023A7BBD73250516F069DF18B500');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
